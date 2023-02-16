43e837bb6f43ebe67e515a8604ac0fd8fc3ab3eb
Michael Zevin <michael.j.zevin@gmail.com>
2023-02-16 10:54:25 -0600

    added submit scripts

--- submit_scripts/calculate_eccentricities.sh
+++ submit_scripts/calculate_eccentricities.sh
@@ -2,7 +2,7 @@
 
 python ./calculate_eccentricities.py \
 --data-path /project2/kicp/michaelzevin/eccentricity/redshift_evolution/data/clusters_det.h5 \
---output-path './eccentricities_redshifting.hdf5' \
+--output-path './eccentricities_noredshifting.hdf5' \
 --fLow 10 3 \
 --multiproc 8 \
-#--calc-with-no-redshifting
+--calc-with-no-redshifting
